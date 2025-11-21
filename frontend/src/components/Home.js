93ac5714c9d3a70f1e87f51495374a165c9e2be4
Aishwarya Patil <aish.p411@gmail.com>
2025-11-21 16:48:23 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,35 +5,45 @@ function Home() {
   const [message, setMessage] = useState("");
   const [profile, setProfile] = useState([]);
 
-  // Read backend URLs from environment variables
-  const HELLO_API = process.env.REACT_APP_HELLO_API;
-  const PROFILE_API = process.env.REACT_APP_PROFILE_API;
+  const helloBaseUrl = process.env.REACT_APP_HELLO_BASE_URL;
+  const profileBaseUrl = process.env.REACT_APP_PROFILE_BASE_URL;
 
   useEffect(() => {
     axios
-      .get(`${HELLO_API}/`)
-      .then((response) => setMessage(response.data.msg))
+      .get(`${helloBaseUrl}/`)
+      .then((response) => {
+        setMessage(response.data.msg);
+      })
       .catch((error) => console.error("Error fetching data:", error));
-  }, [HELLO_API]);
+  }, []);
 
   useEffect(() => {
     axios
-      .get(`${PROFILE_API}/fetchUser`)
-      .then((response) => setProfile(response.data))
+      .get(`${profileBaseUrl}/fetchUser`)
+      .then((response) => {
+        setProfile(response.data);
+        
+      })
       .catch((error) => console.error("Error fetching data:", error));
-  }, [PROFILE_API]);
+  },[]);
+
+  
 
   return (
     <div className="App">
       <h1>{message}</h1>
       <div>
         <h2>Profile</h2>
-        {profile.map((user) => (
-          <div key={user._id}>
-            <h3>Name: {user.name}</h3>
-            <h3>Age: {user.age}</h3>
-          </div>
-        ))}
+        {
+        profile.map((user) => {
+            console.log('user', user)
+          return (
+            <div>
+              <h3>Name: {user.name}</h3>
+              <h3>Age: {user.age}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
